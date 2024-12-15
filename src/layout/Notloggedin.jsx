2ff7ee49f4a85a76d3fee128e5fcb3ec5b7b1db1
vishnu-1817 <139904717+vishnu-1817@users.noreply.github.com>
2024-12-15 16:30:10 +0530

    made Notloggedin page

---
+++ src/layout/Notloggedin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notloggedin = () => {
+  return (
+    <div>
+      <h1>Please Log In</h1>
+      <h1>If you don't have an account, please register first.</h1>
+    </div>
+  )
+}
+
+export default Notloggedin
