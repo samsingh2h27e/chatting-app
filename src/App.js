c358ef1e85eb911ba0ecfa1ca424757b2b7bbe95
vishnu-1817 <139904717+vishnu-1817@users.noreply.github.com>
2024-12-15 12:59:07 +0530

    set the page routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Register from './register';
+import {BrowserRouter,  Routes, Route } from "react-router-dom"
 import Login from './Login';
+import Home from "./Home";
 function App() {
     // return <Register />
-    return <Login></Login>
+    // return <Login></Login>
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+                <Route path="/register" element={<Register/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
