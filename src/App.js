7f9505c6c3e3ce44fabc2b31e01ea50731344247
vishnu-1817 <139904717+vishnu-1817@users.noreply.github.com>
2024-12-15 12:30:07 +0530

    commit 0 , just for testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Register from './register';
+import Login from './Login';
 function App() {
-    return <Register />
+    // return <Register />
+    return <Login></Login>
 }
 
 export default App;
